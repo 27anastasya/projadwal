07f7723bfa28000c152439ce021035a98a622842
yogamatcha10 <yogainformatika10@gmail.com>
2023-06-14 17:57:15 +0700

    lanjutan 14 edit selesai

--- resources/views/jadwals/edit.blade.php
+++ resources/views/jadwals/edit.blade.php
@@ -7,109 +7,163 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Edit Jadwal</div>
-                    @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                    @endif
-                    <form action="{{ route('jadwals.update',$jadwal->id) }}" method="POST" enctype="multipart/form-data">
-                        @csrf
-                        @method('PUT')
-                        <div class="card-body">
-                            <div class="col-md-8">
+                <form action="{{ route('jadwals.update',$jadwal->id) }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    @method('PUT')
+                    <div class="card-body">
+                        <div class="col-md-8">
+                            <div class="form-group">
+                                <strong>Nama Kelas:</strong>
+                                <input type="text" name="nama_kelas" class="form-control" placeholder="Nama Kelas" value="{{ $jadwal->nama_kelas }}">
+                                @error('nama_kelas')
+                                <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                                @enderror
+                            </div>
+
+                            <div class="col-xs-12 col-sm-12 col-md-12">
                                 <div class="form-group">
-                                    <strong>Nama Kelas:</strong>
-                                    <input type="text" name="nama_kelas" class="form-control" placeholder="Nama Kelas">
-                                    @error('nama_kelas')
+                                    <strong>Ruangan:</strong>
+                                    <input type="text" name="ruangan" class="form-control" placeholder="Ruangan" value="{{ $jadwal->ruangan }}">
+                                    @error('ruangan')
                                     <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                                     @enderror
                                 </div>
+                            </div>
 
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <strong>Ruangan:</strong>
-                                        <input type="text" name="ruangan" class="form-control" placeholder="Ruangan">
-                                        @error('ruangan')
-                                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-                                </div>
-
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <strong>Nama Dosen:</strong>
-                                        <input type="text" name="nama_dosen" class="form-control" placeholder="Nama Dosen">
-                                        @error('nama_dosen')
-                                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-                                </div>
-
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <strong>Hari:</strong>
-                                        <input type="date" name="tanggal" class="form-control" placeholder="Tanggal">
-                                        @error('tanggal')
-                                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-                                </div>
-                                <div class="row col-xs-12 col-sm-12 col-md-12 mt-3">
-                                    <div class="col-md-10 form-group">
-                                        <input type="text" name="search" id="search" class="form-control" placeholder="Masukkan Nama Mahasiswa">
-                                        @error('search')
-                                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-                                    <div class="col-md-2 form-group text-center">
-                                        <button class="btn btn-secondary" type="button" name="btnAdd" id="btnAdd">
-                                            <i class="fa fa-plus"></i> Tambah
-                                        </button>
-                                    </div>
-                                </div>
-
-                                <div class="col-xs-12 col-sm-12 col-md-12 mt-3">
-                                    <table id="example" class="table table-striped" style="width:100%">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col">No</th>
-                                                <th scope="col">NIM</th>
-                                                <th scope="col">Nama Mahasiswa</th>
-                                                <th scope="col">Mata Kuliah</th>
-                                                <th scope="col">Jumlah Sks</th>
-                                                <th scope="col">Action</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody id="detail">
-
-                                        </tbody>
-                                    </table>
-                                </div>
-
-                                <div class="col-xs-12 col-sm-12 col-md-12">
-                                    <div class="form-group">
-                                        <strong>Jumlah Mahasiswa:</strong>
-                                        <input type="text" name="jml" class="form-control" placeholder="Jumlah Mahasiswa">
-                                        @error('jml')
-                                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-                                </div>
-                                <br>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
                                 <div class="form-group">
-                                    <button type="submit" class="btn btn-primary ml-3">Submit</button>
-                                    <a class="btn btn-danger" href="{{ route('jadwals.index') }}">Back</a>
+                                    <strong>Nama Dosen:</strong>
+                                    <input type="text" name="nama_dosen" class="form-control" placeholder="Nama Dosen" value="{{ $jadwal->nama_dosen }}">
+                                    @error('nama_dosen')
+                                    <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                                    @enderror
                                 </div>
                             </div>
+
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Hari:</strong>
+                                    <input type="date" name="tanggal" class="form-control" placeholder="Tanggal" value="{{ $jadwal->tanggal }}">
+                                    @error('tanggal')
+                                    <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="row col-xs-12 col-sm-12 col-md-12 mt-3">
+                                <div class="col-md-10 form-group">
+                                    <input type="text" name="search" id="search" class="form-control" placeholder="Masukkan Nama Mahasiswa">
+                                    @error('search')
+                                    <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div class="col-md-2 form-group text-center">
+                                    <button class="btn btn-secondary" type="button" name="btnAdd" id="btnAdd">
+                                        <i class="fa fa-plus"></i> Tambah
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-12 col-md-12 mt-3">
+                                <table id="example" class="table table-striped" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">No</th>
+                                            <th scope="col">NIM</th>
+                                            <th scope="col">Nama Mahasiswa</th>
+                                            <th scope="col">Mata Kuliah</th>
+                                            <th scope="col">Jumlah Sks</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="detail">
+
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <div class="form-group">
+                                    <strong>Jumlah Mahasiswa:</strong>
+                                    <input type="text" name="jml" class="form-control" placeholder="Jumlah Mahasiswa">
+                                    @error('jml')
+                                    <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary mt-3 ml-3">Submit</button>
                         </div>
-                    </form>
-                </div>
+                    </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    </form>
+    @endsection
+
+    @section('js')
+    <script type="text/javascript">
+        var path = "{{ route('search.mahasiswa') }}";
+
+        $("#search").autocomplete({
+            source: function(request, response) {
+                $.ajax({
+                    url: path,
+                    type: 'GET',
+                    dataType: "json",
+                    data: {
+                        search: request.term
+                    },
+                    success: function(data) {
+                        response(data);
+                    }
+                });
+            },
+            select: function(event, ui) {
+                $('#search').val(ui.item.label);
+                console.log($("input[name=jml]").val());
+                if ($("input[name=jml]").val() > 0) {
+                    for (let i = 1; i <= $("input[name=jml]").val(); i++) {
+                        id = $("input[name=id_mahasiswa" + i + "]").val();
+                        if (id == ui.item.id) {
+                            alert(ui.item.value + ' sudah ada!');
+                            break;
+                        } else {
+                            add(ui.item.id);
+                        }
+                    }
+                } else {
+                    add(ui.item.id);
+                }
+                return false;
+            }
+        });
+
+        function add(id) {
+            const path = "{{ route('mahasiswa.index') }}/" + id;
+            var html = "";
+            var no = 0;
+            if ($('#detail tr').length > no) {
+                html = $('#detail').html();
+                no = no + $('#detail tr').length;
+            }
+            $.ajax({
+                url: path,
+                type: 'GET',
+                dataType: "json",
+                success: function(data) {
+                    console.log(data);
+                    no++;
+                    html += '<tr>' +
+                        '<td>' + no + '<input type="hidden" name="id' + no + '" class="form-control" value="' + data.id + '"></td>' +
+                        '<td><input type="text" name="nim' + no + '" class="form-control" value="' + data.nim + '"></td>' +
+                        '<td><input type="text" name="nama_mahasiswa' + no + '" class="form-control" value="' + data.nama_mahasiswa + '"></td>' +
+                        '<td><input type="text" name="mata_kuliah' + no + '" class="form-control"' + data.mata_kuliah + '"></td>' +
+                        '<td><input type="text" name="jumlah_sks' + no + '" class="form-control"' + data.jumlah_sks + '"></td>' +
+                        '<td><a href="#" class="btn btn-sm btn-danger">Delete</a></td>' +
+                        '</tr>';
+                    $('#detail').html(html);
+                    $('input[name=jml]').val(no);
+                }
+            });
+        }
+    </script>
+    @endsection
